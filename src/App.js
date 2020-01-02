02a70e21c8e0cc9c7ed500f448bbf8ec8e3dbde6
abdullah <abdullahmafuz1@gmail.com>
2020-01-02 17:11:53 +0400

    init setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React,{Fragment} from 'react';
 
 import './App.scss';
-import starwarsLogo from './img/Star_Wars_Logo.svg';
+
+
+import Login from '../src/components/auth/Login'
 
 const App=()=> {
   return (
     <Fragment>
-      <h1>react app ready</h1>
-      <img src={starwarsLogo}></img>
+      <Login />
     </Fragment>
   );
 }
